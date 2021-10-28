9c466ab17ee7f6badbe2f4ba1bfda8ad2f142578
xuecan yan <yxcyanxuecan@163.com>
2021-10-28 20:55:12 +0800

    modify allowed host

--- acapp/settings.py
+++ acapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%x1oej#@w5n*uo%)viea!il*d^5e$gz#lzzad!puk3795mdc#j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["47.100.238.236"]
 
 
 # Application definition
