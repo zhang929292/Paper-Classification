882ff6bf33ff8bf276a9c42f11dce0d8acbfbae3
yanxuecan <yxcyanxuecan@163.com>
2021-11-30 22:23:49 +0800

    implement multiplayer section-1

--- acapp/settings.py
+++ acapp/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["47.100.238.236", "app165.acapp.acwing.com.cn"]
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'game.apps.GameConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -140,3 +141,16 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ASGI_APPLICATION = 'acapp.asgi.application'
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
+ROOM_CAPACITY = 3
